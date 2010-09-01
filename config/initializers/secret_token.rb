92098f3a1451017af42c19d2d0ae4da436e107a7
yalab <rudeboyjet@gmail.com>
2010-09-01 19:14:16 +0900

    git init; git add . git commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MinamirbRails3::Application.config.secret_token = 'ee3f1008cf4013ba02bc2e6f92531aed00b6d92208e065368b315615f8e5291eab4aa957af4efa2a32d35b1df16ecaca1bd1aa6f5af0ce2f9c4feccf4f9a6610'
